a05a9b75e6d4a75c02745fea1ba4aaa8221a0a21
Sergio Rios <sergioriosg@gmail.com>
2011-09-30 23:28:34 +0000

    Carga inicial HotelNet

--- mtcol/library/My/View/Helper/DynamicResources.php
+++ mtcol/library/My/View/Helper/DynamicResources.php
@@ -6,20 +6,15 @@ class My_View_Helper_DynamicResources extends Zend_View_Helper_Abstract {
         $view = Zend_Controller_Front::getInstance()->getRequest()->getActionName();        
         $filejs = $controller.'_'.$view.'.js';
         $filecss = $controller.'_'.$view.'.css';
-        $urljs = APPLICATION_PATH."/../public/js/".$module."/".$filejs;
-        $urljsui = APPLICATION_PATH."/../public/js/".$module."/ui/".$filejs;
+        $urljs = APPLICATION_PATH."/../public/js/".$module."/".$filejs;        
         $urlcss = APPLICATION_PATH."/../public/css/".$module."/".$filecss;
         
         
-        $scriptjs  = (file_exists($urljs) && file_exists($urljsui))?'
+        $scriptjs  = (file_exists($urljs))?'
             var script = document.createElement("script");
             script.type = "text/javascript";
             script.src = "/js/'.$module.'/'.$filejs.'";
-            document.getElementsByTagName("head")[0].appendChild(script);
-            var scriptui = document.createElement("script");
-            scriptui.type = "text/javascript";
-            scriptui.src = "/js/'.$module.'/ui/'.$filejs.'";
-            document.getElementsByTagName("head")[0].appendChild(scriptui);
+            document.getElementsByTagName("head")[0].appendChild(script);            
             ':'';        
         
         $scripcss = (file_exists($urlcss))?'
@@ -32,9 +27,9 @@ class My_View_Helper_DynamicResources extends Zend_View_Helper_Abstract {
         if(!empty($scriptjs) || !empty($scripcss))
             $script = '<script type="text/javascript">
                         Ext.onReady(function(){
-                        '.$scripcss.'
-                        '.$scriptjs.'
                         Ext.Loader.setConfig({enabled:true});
+                        '.$scripcss.'
+                        '.$scriptjs.'                        
                 });    
             </script>';
         
--- mtcol/library/My/View/Helper/Extjs.php
+++ mtcol/library/My/View/Helper/Extjs.php
@@ -1,8 +1,8 @@
 <?php
 class My_View_Helper_Extjs{
     public function extjs(){
-        $script = '<link href="/extjs/resources/css/ext-all.css" rel="stylesheet" />';
-        $script .= '<script type="text/javascript" src="/extjs/ext-all-debug.js"></script>';
+        $script = '<link href="/ext-4.0.2a/resources/css/ext-all.css" rel="stylesheet" />';
+        $script .= '<script type="text/javascript" src="/ext-4.0.2a/ext-all-debug.js"></script>';
         return $script;
     }
 }
