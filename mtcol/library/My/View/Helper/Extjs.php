b5d0a9e6edf262675e99e4633e2a656271ac2a2d
Sergio Rios <sergioriosg@gmail.com>
2012-01-12 22:24:28 +0000

    Cambio de esquema de colores

--- mtcol/library/My/View/Helper/Extjs.php
+++ mtcol/library/My/View/Helper/Extjs.php
@@ -1,7 +1,7 @@
 <?php
 class My_View_Helper_Extjs extends Zend_View_Helper_Abstract{
     public function extjs(){
-        $script = '<link href="/ext-4.0.2a/resources/css/ext-all.css" rel="stylesheet" />';
+        $script = '<link href="/ext-4.0.2a/resources/css/ext-all-gray.css" rel="stylesheet" />';
         $script .= '<script type="text/javascript" src="/ext-4.0.2a/ext-all.js"></script>';
         return $script;
     }
